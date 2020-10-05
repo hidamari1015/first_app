98d793f163e43194c709349c276f2d35f48ac10e
hidamari1015 <86.sano.masahiro@gmail.com>
2020-10-05 19:19:00 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+  def index #indexアクションを定義した
+    #@post = Post.find(1) 1番目のレコードを＠postに代入
+    @posts = Post.all #すべてのレコードを＠postに代入
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+end
